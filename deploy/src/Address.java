2d309d35eb51389a04f79175066b57a6a3ce33d1
Lucas <lucas.schraven@student.hu.nl>
2017-02-07 17:05:51 +0100

    Opdracht 2 toegevoegd

---
+++ deploy/src/Address.java
@@ -0,0 +1,17 @@
+/**
+ * Created by lucas on 7-2-2017.
+ */
+public class Address {
+
+    private String street, zip, city;
+
+
+    public void setStreet(String strt){street = strt;}
+    public String getStreet(){return street;}
+    public void setZip(String zp){zip = zp;}
+    public String getZip(){return zip;}
+    public void setCity(String ct){city = ct;}
+    public String getCity(){return city;}
+
+
+}
--- deploy/src/JaxbMain1.java
+++ deploy/src/JaxbMain1.java
@@ -1,7 +1,7 @@
 /**
  * Created by lucas on 7-2-2017.
  */
-public class Main {
+public class JaxbMain1 {
     public static void main(String[] args) {
         System.out.println("Hello IAC");
     }
